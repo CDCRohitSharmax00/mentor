08e96c43c8027bc27e010591c413c09bf2431724
rohit-rambade <rohitrambade18@gmail.com>
2024-01-30 17:22:50 +0530

    creating profile based on role

--- Backend-Assets/controllers/user.controller.js
+++ Backend-Assets/controllers/user.controller.js
@@ -1,23 +1,37 @@
 const User = require("../models/User");
+const MenteeProfile = require("../models/menteeProfile");
 const MentorProfile = require("../models/mentorProfile");
 
 const createProfile = async (req, res) => {
   /* userId & role will come from decoded token */
   const { userId, role } = req.body;
   console.log(userId, role);
-  const user = await User.findById(userId);
+  const user = await User.findOne({ _id: userId, role });
   console.log("DB User", user);
   if (!user) {
     return res.status(404).json({ error: "User not found" });
   }
   // const updatedProfileFields = { bio, expertise, yoe };
-  const updatedProfile = await MentorProfile.findByIdAndUpdate(
-    user.details,
-    req.body,
-    { new: true }
-  );
-  console.log(updatedProfile);
-  res.json({ data: updatedProfile });
+
+  if (user.role === "mentor") {
+    const updatedProfile = await MentorProfile.findByIdAndUpdate(
+      user.details,
+      req.body,
+      { new: true }
+    );
+    console.log("logic", updatedProfile);
+    return res.json({ data: updatedProfile });
+  }
+
+  if (user.role === "mentee") {
+    const updatedProfile = await MenteeProfile.findByIdAndUpdate(
+      user.details,
+      req.body,
+      { new: true }
+    );
+    console.log(updatedProfile);
+    return res.json({ data: updatedProfile });
+  }
 };
 
 const getAllMentors = async (req, res) => {
